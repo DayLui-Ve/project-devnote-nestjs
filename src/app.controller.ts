2645dcd4d610055d1e1b882e6f509b657f088a86
Luis Briceno <luijo265@gmail.com>
2020-09-17 21:23:25 -0500

    add validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,33 @@
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { AppService, ICourseAttributes } from './app.service';
+import CreateCourseDto from './course/create-course-dto';
 
 @Controller()
 export class AppController {
-
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getCourses(){
-    return this.appService.getCourses()
+  getCourses() {
+    return this.appService.getCourses();
   }
-  
+
   @Get(':id')
-  findCourse(@Param('id') id:number){
-    return this.appService.findCourse(id)
+  findCourse(@Param('id') id: number) {
+    return this.appService.findCourse(id);
   }
 
   @Post()
-  createCourse(@Body() course:ICourseAttributes){
-    return this.appService.createCourse(course)
+  createCourse(@Body() course: CreateCourseDto) {
+    return this.appService.createCourse(course);
   }
 
   @Put(':id')
-  updateCourse(@Param('id') id:number, @Body() course:ICourseAttributes){
-    return this.appService.updateCourse(id, course)
+  updateCourse(@Param('id') id: number, @Body() course: ICourseAttributes) {
+    return this.appService.updateCourse(id, course);
   }
 
   @Delete(':id')
-  deleteCourse(@Param('id') id:number){
-    return this.appService.deleteCourse(id)
+  deleteCourse(@Param('id') id: number) {
+    return this.appService.deleteCourse(id);
   }
 }
---
+++ src/course/create-course-dto.ts
@@ -0,0 +1,29 @@
+import { IsString, MaxLength, IsNotEmpty, IsEnum } from 'class-validator';
+import { State } from '../app.service';
+
+class CreateCourseDto {
+  @IsString({
+    message: messageProperty('debe ser una cadena de texto'),
+  })
+  @MaxLength(20, {
+    message: messageProperty('debe ser máximo $constraint1 caracteres'),
+  })
+  @IsNotEmpty({
+    message: messageProperty('es requerido'),
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: messageProperty('es requerido'),
+  })
+  @IsEnum(State, {
+    message: messageProperty('no es válido'),
+  })
+  state: State;
+}
+
+function messageProperty(message: string): string {
+  return `La propiedad $property ${message}`;
+}
+
+export default CreateCourseDto;
